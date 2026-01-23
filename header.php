f11a173e26b7222a9de50cde06eb245be19db1cc
Prathapage L.L. <lahirulakshithaprathapage@gmail.com>
2026-01-23 11:12:11 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,57 @@
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<header class="header">
+
+   <div class="header-1">
+      <div class="flex">
+      <div class="logo">
+                <a href="home.php" title="Logo">
+                    <img src="img/logo.png" alt="Restaurant Logo" class="img-responsive">
+                </a>
+            </div>
+         <p> <a href="login.php">login</a> | <a href="register.php">register</a> </p>
+      </div>
+   </div>
+
+   <div class="header-2">
+      <div class="flex">
+         <a href="home.php" class="logo">WoowfooD</a>
+
+         <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">foods</a>
+            <a href="shop.php">shop</a>
+            <a href="contact.php">contact</a>
+            
+         </nav>
+
+         <div class="icons">
+            
+            <div id="user-btn" class="fas fa-user"></div>
+            <?php
+               $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+               $cart_rows_number = mysqli_num_rows($select_cart_number); 
+            ?>
+            <a href="cart.php"> <i class="fas fa-shopping-cart"></i> <span>(<?php echo $cart_rows_number; ?>)</span> </a>
+         </div>
+
+         <div class="user-box">
+            <p>username : <span><?php echo $_SESSION['user_name']; ?></span></p>
+            <p>email : <span><?php echo $_SESSION['user_email']; ?></span></p>
+            <a href="logout.php" class="delete-btn">logout</a>
+         </div>
+      </div>
+   </div>
+
+</header>
