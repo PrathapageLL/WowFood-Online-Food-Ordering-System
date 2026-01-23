80a57cc4fd1d623afb3a537899aaadeb9e9929e3
Prathapage L.L. <lahirulakshithaprathapage@gmail.com>
2026-01-23 11:12:29 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,45 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> shani@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> No 2.near police station,kuliyapitiya </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+      
+      </div>
+      <iframe 
+                class="map" 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345094146!2d144.95373531564484!3d-37.816279742021524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577c73e1bf0d4c7!2s123%20Foodie%20Lane%2C%20Melbourne%20VIC%203000%2C%20Australia!5e0!3m2!1sen!2sus!4v1605665937727!5m2!1sen!2sus" 
+                width="100%" 
+                height="300" 
+                frameborder="0" 
+                style="border:0;" 
+                allowfullscreen="" 
+                aria-hidden="false" 
+                tabindex="0">
+            </iframe>
+
+   </div>
+
+   
+</section>
